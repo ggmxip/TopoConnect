daf30175f3d5cd76eb4a6ee8feec667d0c163b9d
ggmxip <92012598+ggmxip@users.noreply.github.com>
2024-10-29 02:09:48 +0530

    im done

--- app.py
+++ app.py
@@ -8,33 +8,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-# import streamlit as st
-#
-# # Load CSS
-# def load_css():
-#     with open("static/styles.css") as f:
-#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-#
-# # Load JavaScript
-# def load_js():
-#     with open("static/js/script.js") as f:
-#         st.markdown(f"<script>{f.read()}</script>", unsafe_allow_html=True)
-#
-# # Load HTML
-# def load_html():
-#     with open("templates/index.html") as f:
-#         st.markdown(f"{f.read()}", unsafe_allow_html=True)
-#
-# # Main function
-# def main():
-#     load_css()  # Load CSS
-#     load_js()   # Load JavaScript
-#     load_html() # Load HTML
-#
-# if __name__ == '__main__':
-#     main()
