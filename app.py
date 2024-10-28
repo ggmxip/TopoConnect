d3d9ca5898fb92ba183254c4fd2a5a089b7b04e7
ggmxip <92012598+ggmxip@users.noreply.github.com>
2024-10-29 00:30:12 +0530

    revert to flask lol

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-import streamlit as st
+from flask import Flask, render_template
 
-# Load CSS
-def load_css():
-    with open("static/styles.css") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+app = Flask(__name__)
 
-# Load JavaScript
-def load_js():
-    with open("static/js/script.js") as f:
-        st.markdown(f"<script>{f.read()}</script>", unsafe_allow_html=True)
-
-# Load HTML
-def load_html():
-    with open("templates/index.html") as f:
-        st.markdown(f"{f.read()}", unsafe_allow_html=True)
-
-# Main function
-def main():
-    load_css()  # Load CSS
-    load_js()   # Load JavaScript
-    load_html() # Load HTML
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
+
+
+
+
+
+# import streamlit as st
+#
+# # Load CSS
+# def load_css():
+#     with open("static/styles.css") as f:
+#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+#
+# # Load JavaScript
+# def load_js():
+#     with open("static/js/script.js") as f:
+#         st.markdown(f"<script>{f.read()}</script>", unsafe_allow_html=True)
+#
+# # Load HTML
+# def load_html():
+#     with open("templates/index.html") as f:
+#         st.markdown(f"{f.read()}", unsafe_allow_html=True)
+#
+# # Main function
+# def main():
+#     load_css()  # Load CSS
+#     load_js()   # Load JavaScript
+#     load_html() # Load HTML
+#
+# if __name__ == '__main__':
+#     main()
